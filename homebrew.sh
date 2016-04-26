522c0eed53adc59ccf19c40c9c038d1181b39aab
Moses Mendoza <moses@puppetlabs.com>
2016-04-26 07:36:16 -0700

    Initial homebrew instal

---
+++ homebrew.sh
@@ -0,0 +1,5 @@
+# Install homebrew and the apps I'm interested in
+
+install_homebrew(){
+  yes \r | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+}
