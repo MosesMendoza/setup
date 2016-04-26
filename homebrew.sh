f575cc3997654340b51c031f04c3d37edea5c6b5
Moses Mendoza <moses@puppetlabs.com>
2016-04-26 08:11:30 -0700

    Add brews to install with homebrew

--- homebrew.sh
+++ homebrew.sh
@@ -3,3 +3,16 @@
 install_homebrew(){
   yes \r | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 }
+
+install_brews(){
+  brews="ack bash-completion git htop-osx iterm2 ruby tree vim wget"
+
+  for brew in $brews; do
+    if ! brew list $brew >/dev/null 2>&1; then
+      echo "installing $brew..."
+      brew install $brew
+    else
+      echo "$brew already installed. Skipping..."
+    fi
+  done
+}
